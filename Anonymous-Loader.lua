d22368aaa6dd24a86e5a94ecc036263f458afbc4
UncensoredUsers <boomdiscordbot@gmail.com>
2024-12-01 15:29:17 +0900

    Added MM2 and Optimized the script

--- Anonymous-Loader.lua
+++ Anonymous-Loader.lua
@@ -1,9 +1,8 @@
--- v0.0.5 loader
 local placeId = game.PlaceId
 
 local games = {
     [16732694052] = "https://raw.githubusercontent.com/UncensoredUsers/Anonymous-X/main/Fisch/Fisch.lua", -- Fisch
-    [0] = "",
+    [142823291] = "https://raw.githubusercontent.com/UncensoredUsers/Anonymous-X/main/MurderMystery2/MM2.lua", -- Murder Mystery 2 (Developing. Not Really)
 }
 
 local startTime = tick()
@@ -12,7 +11,6 @@ if games[placeId] ~= "" then
     local scriptContent = game:HttpGet(games[placeId])
     loadstring(scriptContent)()
 end
-local endTime = tick()
 
-local elapsedTime = endTime - startTime
+local elapsedTime = tick() - startTime
 print("Loaded. Elapsed Time: " .. string.format("%.2f", elapsedTime) .. " seconds.")
