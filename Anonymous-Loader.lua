220c45237db98cc2e362dbed8a9cc02678db845b
UncensoredUsers <boomdiscordbot@gmail.com>
2024-12-01 15:58:35 +0900

    Added Error Print Message

--- Anonymous-Loader.lua
+++ Anonymous-Loader.lua
@@ -7,10 +7,28 @@ local games = {
 
 local startTime = tick()
 
-if games[placeId] ~= "" then
-    local scriptContent = game:HttpGet(games[placeId])
-    loadstring(scriptContent)()
-end
+local success, errorMessage = pcall(function()
+    if games[placeId] then
+        local scriptContent, scriptError = game:HttpGet(games[placeId], true)
+        
+        if not scriptContent then
+            error("HTTP GET failed: " .. scriptError)
+        end
+
+        local loadSuccess, loadError = pcall(loadstring(scriptContent))
+        if not loadSuccess then
+            error("Loadstring failed: " .. loadError)
+        end
+        loadstring(scriptContent)()
+    else
+        error("Game not found in the list.")
+    end
+end)
 
 local elapsedTime = tick() - startTime
-print("Loaded. Elapsed Time: " .. string.format("%.2f", elapsedTime) .. " seconds.")
+
+if not success then
+    warn("Error occurred: " .. errorMessage)
+else
+    print("Loaded. Elapsed Time: " .. string.format("%.2f", elapsedTime) .. " seconds.")
+end
